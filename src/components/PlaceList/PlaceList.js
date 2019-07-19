a5aa2f2e4e2a49f2d4fcf052e5329846ea6aacfa
Davi <davi.guimaraesleite@gmail.com>
2019-07-19 16:04:16 -0300

    reacting to press events.

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -5,7 +5,7 @@ import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
     const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place} onItemPressed={() => alert('Item Pressed - ID: ' + i)} />
+        <ListItem key={i} placeName={place} onItemPressed={() => props.onItemDeleted(i)} />
     ));
     
     return (
