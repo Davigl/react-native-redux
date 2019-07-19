fccbe48c2e87aa125c2709da3be89418104106d0
Davi <davi.guimaraesleite@gmail.com>
2019-07-19 16:27:41 -0300

    rendering lists correctly.

--- App.js
+++ App.js
@@ -6,11 +6,11 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput'
-import PlaceList from './src/components/PlaceList/PlaceList'
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
 
 export default class App extends Component {
   state = {
@@ -26,37 +26,43 @@ export default class App extends Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(),
+          value: placeName
+        })
       };
     });
   };
 
-  placeDeletedHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
-      }
+      };
     });
-  }
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
-  };
-};
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    alignItems: "center"
   }
 });
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,21 +1,21 @@
-import React from 'React';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "React";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const listItem = (props) => (
-    <TouchableOpacity onPress={props.onItemPressed}>    
-        <View style={styles.listItem}>
-            <Text> {props.placeName} </Text>
-        </View>
-    </TouchableOpacity>
+const listItem = props => (
+  <TouchableOpacity onPress={props.onItemPressed}>
+    <View style={styles.listItem}>
+      <Text> {props.placeName} </Text>
+    </View>
+  </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
-    listItem: {
-        width: "100%",
-        padding: 10,
-        backgroundColor: "#eee",
-        marginBottom: 5
-    }
+  listItem: {
+    width: "100%",
+    padding: 10,
+    backgroundColor: "#eee",
+    marginBottom: 5
+  }
 });
 
 export default listItem;
