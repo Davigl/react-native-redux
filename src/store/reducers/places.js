dcd42e4b281f48583199c766d73647997d7ad8f9
Davi <davi.guimaraesleite@gmail.com>
2019-07-23 17:03:52 -0300

    setting up the reducer.

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,10 @@
+import {
+  ADD_PLACE,
+  DELETE_PLACE,
+  DESELECT_PLACE,
+  SELECT_PLACE
+} from "../actions/actionTypes";
+
 const initialState = {
   places: [],
   selectedPlace: null
@@ -5,6 +12,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: prevState.places.concat({
+          key: Math.random(),
+          name: placeName,
+          image: {
+            uri:
+              "https://img.theculturetrip.com/x/smart/wp-content/uploads/2019/04/shutterstock_421013719.jpg"
+          }
+        })
+      };
     default:
       return state;
   }
